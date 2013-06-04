ae412e941a17a9bf78ac62d22160b3ae7125375f
Thomas Baum <thomas.baum@atns.de>
2013-06-04 16:33:09 +0200

    add helper class

---
+++ src/main/java/com/google/inject/extensions/neo4j/ListResult.java
@@ -0,0 +1,25 @@
+package com.google.inject.extensions.neo4j;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class ListResult<E extends Serializable> implements Serializable {
+
+    private final List<E> list;
+    private final int total;
+
+    public ListResult(Collection<E> list, int total) {
+        this.list = new ArrayList<E>(list);
+        this.total = total;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public List<E> getEntries() {
+        return list;
+    }
+}
