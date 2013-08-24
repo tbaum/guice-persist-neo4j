a588ad510072b09c1f2f8f3308c57a5f69aca13f
Thomas Baum <thomas.baum@atns.de>
2013-08-24 11:18:47 +0200

    disable onlinebackup for Impermanent-Test DBs

--- src/main/java/com/google/inject/extensions/neo4j/ImpermanentNeo4jPersistenceModule.java
+++ src/main/java/com/google/inject/extensions/neo4j/ImpermanentNeo4jPersistenceModule.java
@@ -1,10 +1,16 @@
 package com.google.inject.extensions.neo4j;
 
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.factory.GraphDatabaseBuilder;
 import org.neo4j.test.TestGraphDatabaseFactory;
+
+import static org.neo4j.helpers.Settings.BOOLEAN;
+import static org.neo4j.helpers.Settings.setting;
 
 public class ImpermanentNeo4JPersistenceModule extends Neo4JPersistenceModule {
     @Override protected GraphDatabaseService createGraphDatabase() {
-        return new TestGraphDatabaseFactory().newImpermanentDatabase();
+        GraphDatabaseBuilder builder = new TestGraphDatabaseFactory().newImpermanentDatabaseBuilder();
+        builder.setConfig(setting("online_backup_enabled", BOOLEAN, ""), "false");
+        return builder.newGraphDatabase();
     }
 }
